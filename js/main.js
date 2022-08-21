611188f67a13697da31a9151410b98dfdbf46598
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-08-21 17:40:14 -0300

    end first video

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 const input = document.querySelector('.login-input');
 const button = document.querySelector('.login-button');
+const form = document.querySelector('.login-form');
 
-const validarInput = (event)=>{
-    console.log(event.target);
+const validarInput = ({target})=>{
+    if(target.value.length > 2){
+        button.removeAttribute('disabled');
+        return;
+    }
+    button.setAttribute('disabled','');
 }
 
+const handleSubmit =(event) =>{
+    event.preventDefault();
+    localStorage.setItem('player',input.value);
+    window.location ='../html/game.html';
+
+}
 input.addEventListener('input',validarInput);
+form.addEventListener('submit',handleSubmit);
