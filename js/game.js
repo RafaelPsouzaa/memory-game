b0fc659d7da01d130cc3be5dea22e462366f3dd8
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-08-28 11:08:08 -0300

    criaçao de carta

--- js/game.js
+++ js/game.js
@@ -1,5 +1,19 @@
 const grid = document.querySelector('.grid');
 
+const characters = [
+    'beth',
+    'bg',
+    'brain',
+    'jerry',
+    'jessica',
+    'meeseeks',
+    'pessoa-passaro',
+    'pickle-rick',
+    'scroopy',
+    'summer',
+
+];
+
 const createElement = (tag,className) =>{
     const elemement = document.createElement(tag);
     elemement.className = className;
@@ -7,15 +21,24 @@ const createElement = (tag,className) =>{
 
 }
 
-const createCard = () =>{
+const createCard = () => {
     const card = createElement('div','card');
-
     const front = createElement ('div','face front');
     const back = createElement('div','face back');
-
     card.appendChild(front);
     card.appendChild(back);
 
-    // video 19:28
+    // video 26:48
 
+    return card;
 }
+
+const loadGame = () =>{
+
+    characters.forEach((character)=>{
+        const card = createCard();
+        grid.appendChild(card);
+    });
+}
+
+loadGame();
