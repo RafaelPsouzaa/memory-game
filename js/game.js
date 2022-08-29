87a433740d6af519b3dcf9db8b0965c102413d1a
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-08-29 11:54:53 -0300

    regra de decisao

--- js/game.js
+++ js/game.js
@@ -21,22 +21,57 @@ const createElement = (tag,className) =>{
 
 }
 
-const createCard = () => {
+let firstCard = '';
+let secondCard ='';
+
+const checkCards = ()=>{
+
+}
+
+
+const revealCard = ({target}) => {
+
+    if(target.parentNode.className.includes('reveal-card')){
+        return;
+    }   
+    
+    if(firstCard === ''){
+        target.parentNode.className.includes('reveal-card');
+        firstCard = target.parentNode;
+
+    }else if(secondCard ===''){
+        target.parentNode.className.includes('reveal-card');
+        secondCard = target.parentNode;
+        checkCards();
+    }
+    
+
+   
+}
+
+const createCard = (character) => {
     const card = createElement('div','card');
     const front = createElement ('div','face front');
     const back = createElement('div','face back');
+
+    front.style.backgroundImage = `url('../assets/${character}.png')`;
+
     card.appendChild(front);
     card.appendChild(back);
 
-    // video 26:48
+    card.addEventListener('click',revealCard)
+    card.setAttribute('data-character',character)
 
-    return card;
+return card;
 }
+ // video 47:13
 
 const loadGame = () =>{
 
-    characters.forEach((character)=>{
-        const card = createCard();
+    const duplicateCharacters = [...characters,...characters];
+    const shuffledArray = duplicateCharacters.sort(()=> Math.random()- 0.5);
+    shuffledArray.forEach((character)=>{
+        const card = createCard(character);
         grid.appendChild(card);
     });
 }
