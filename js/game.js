2a20ee23862e73b667553278cc614f43c5c548d5
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-08-30 10:55:57 -0300

    end of the game

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid');
+const spanPlayer=document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
 const characters = [
     'beth',
@@ -28,7 +30,8 @@ const checkEndGame = () =>{
     const disabledCards = document.querySelectorAll('.disabled-card');
 
     if(disabledCards.length === 20){
-        alert('Parabéns,você conseguiu !');
+        clearInterval(this.loop);
+        alert(`Parabens${spanPlayer.innerHTML}! seu tempo foi ${timer.innerHTML}`);
     }
 
 }
@@ -107,4 +110,19 @@ const loadGame = () =>{
     });
 }
 
-loadGame();
+const startTimer = () => {
+
+    this.loop = setInterval(() => {
+         const currentTime = +timer.innerHTML;
+         timer.innerHTML = currentTime + 1;
+
+    },1000);
+
+}
+
+window.onload = () => {
+   spanPlayer.innerHTML = localStorage.getItem('player');
+   startTimer();
+    loadGame();
+    
+}
